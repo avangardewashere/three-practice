282c555286d169d2fb9a8e133966591232849088
avangardewashere <avelpanaligan@gmail.com>
2025-01-09 10:52:19 +0800

    add/file-uploads

--- src/components/fileUpload.tsx
+++ src/components/fileUpload.tsx
@@ -1,7 +1,10 @@
 import { ChangeEvent, useState } from "react";
 
+type UploadStatus = "idle" | "uploading" | "successs" | "error";
+
 const FileUpload = () => {
   const [file, setFile] = useState<File | null>(null);
+  const [status, setStatus] = useState<UploadStatus>("idle");
 
   function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
     if (e.target.files) {
@@ -9,7 +12,7 @@ const FileUpload = () => {
     }
   }
   return (
-    <div>
+    <div style={{ display: "flex", gap: "0 4" }}>
       <input
         type="file"
         name="myFile"
@@ -17,6 +20,15 @@ const FileUpload = () => {
         style={{}}
         onChange={handleFileChange}
       />
+
+      {file && (
+        <div style={{ fontSize: "11px", marginBottom: "4px" }}>
+          <p>File name: {file.name}</p>
+          <p>Size: {(file.size / 1024).toFixed(2)} KB</p>
+          <p>Type : {file.type}</p>
+        </div>
+      )}
+      {file && status !== "uploading" && <button>Upload</button>}
     </div>
   );
 };
