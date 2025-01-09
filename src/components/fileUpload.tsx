85092691a2badb50708cbf78ecb759f1ada0009e
avangardewashere <avelpanaligan@gmail.com>
2025-01-09 11:22:32 +0800

    done/file-upload

--- src/components/fileUpload.tsx
+++ src/components/fileUpload.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from "react";
 
-type UploadStatus = "idle" | "uploading" | "successs" | "error";
+type UploadStatus = "idle" | "uploading" | "success" | "error";
 
 const FileUpload = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -9,6 +9,31 @@ const FileUpload = () => {
   function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
     if (e.target.files) {
       setFile(e.target.files[0]);
+    }
+  }
+
+  async function handleFileUpload() {
+    if (!file) return;
+
+    setStatus("uploading");
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      
+      const res =  await fetch("https://httpbin.org/post", {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: file,
+      });
+
+      console.log(res)
+      setStatus("success")
+    } catch {
+        setStatus("error")
     }
   }
   return (
@@ -28,7 +53,20 @@ const FileUpload = () => {
           <p>Type : {file.type}</p>
         </div>
       )}
-      {file && status !== "uploading" && <button>Upload</button>}
+      {file && status !== "uploading" && <button  onClick={handleFileUpload}>Upload</button>}
+
+      {status === "error" && (
+        <p style={{ color: "red" }}>Upload failed. Please try again</p>
+      )}
+
+      
+
+      {status === "success" && (
+        <p style={{ color: "green" }}>Success</p>
+      )}
+      {status === "uploading" && (
+        <p style={{ color: "green" }}>Uploading</p>
+      )}
     </div>
   );
 };
